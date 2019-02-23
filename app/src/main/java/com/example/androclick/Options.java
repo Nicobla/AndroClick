eb4a7476bd57a66061b29e799cf79e011e620394
Nicobla <nicolas.blachere@etu.univ-smb.fr>
2019-02-23 11:34:46 +0100

    Base de l'application
     - Base écran mes recettes
     - Base écran options

--- app/src/main/java/com/example/androclick/Options.java
+++ app/src/main/java/com/example/androclick/Options.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Options extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_options);
     }
 
-    public void moveToOptions(View view) {
-        startActivity(new Intent(this, Options.class));
+    public void moveToMyrecipes(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
